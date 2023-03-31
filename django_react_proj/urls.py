836778ceddfed554e7f80e164908a48c3feb0263
elitesuper <tonylobbin7712@gmail.com>
2023-03-31 12:46:26 -0700

    change the structure

--- django_react_proj/urls.py
+++ django_react_proj/urls.py
@@ -1,4 +1,4 @@
-"""sma_app URL Configuration
+"""django_react_proj URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
